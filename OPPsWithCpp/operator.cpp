88fcc1aaba798e5d4d6d17c189d27549284397a8
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2024-12-08 18:03:57 +0530

    Operation Added In Cpp

---
+++ OPPsWithCpp/operator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num1 = 10;
+    int num2 = 20;
+
+    int sum = num1 + num2;
+    int sub = num1 - num2;
+    int div = num1 / num2;
+    int mul = num1 * num2;
+
+    cout<< "The Sum Is : "<<sum <<endl;
+    cout<< "The Sub Is : "<<sub <<endl;
+    cout<< "The Div Is : "<<div <<endl;
+    cout<< "The Mul Is : "<<mul <<endl;
+
+    return 0;
+}
