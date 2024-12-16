32c7d74d9773915209c4cc8eaeeaf7ee4cb2ac0b
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2024-12-16 19:50:04 +0530

    Project Class Update

--- advancedPython/projectOfClass.py
+++ advancedPython/projectOfClass.py
@@ -1,32 +1,47 @@
-# Creating Employee Class
-class Employee:
-    def __init__(self,id,name):
-        self.id = id    
-        self.name = name
-
-    def display(self):
-        print("ID : %d\nName : %s"%(self.id,self.name))
-
-    def __del__(self):
-        print("Object Destroyed")
-
-# Taking Input For Number Of Employees
-numOfEmployees = 0
-
-# Handling Invalid Input
-while numOfEmployees < 1:
-   numOfEmployees = int(input("Enter Number Of Employees : "))
 
 # Creating List Of Employees
 employees = []
 
-# Taking Input For Each Employee
-for i in range(numOfEmployees):
-    print("Employee %d"%(i+1))
-    id = int(input("Enter ID : "))
-    name = input("Enter Name : ")
-    employees.append(Employee(id,name))
+# Creating Employee Class
+class Employee:
+    def __init__(self,name):
+        self.id = "emp"+ str(len(employees)) 
+        self.name = name
 
-# Displaying Each Employee
-for employee in employees:
-    employee.display()
+    def display(self):
+        print("ID : %s\nName : %s"%(self.id,self.name))
+
+    def __del__(self):
+        print("Object Destroyed")
+
+# Taking Choice
+choice = "y"
+
+def employeeInput():
+    # Taking Input For Number Of Employees
+    numOfEmployees = 0
+
+    # Handling Invalid Input
+    while numOfEmployees < 1:
+      numOfEmployees = int(input("Enter Number Of Employees : "))
+
+
+
+    # Taking Input For Each Employee
+    for i in range(numOfEmployees):
+        print("Employee %d"%(i+1))
+        name = input("Enter Name : ")
+        employees.append(Employee(name))
+
+    # Displaying Each Employee
+    for employee in employees:
+        employee.display()
+
+    # Asking If User Wants To Continue
+    global choice
+    choice = input("Enter Your Choice (y/n) : ").lower()
+
+
+
+while "y"== choice:
+    employeeInput()
