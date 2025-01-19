66a6164851830100b253fdeefc89b2dcfbe9c958
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2025-01-19 12:09:46 +0530

    Double Linked List Updated

--- DAA/Practical Code/doubleLinkedList.py
+++ DAA/Practical Code/doubleLinkedList.py
@@ -19,26 +19,62 @@ class DoubleLinkedList:
 
         return True
 
-    def insert(self,prevNode,data):
+    def insert(self,key,data):
         curr = self.head
+        
+        # To Handling Base Case
+        if curr.data == key:
+            self.push(data)
+            return True
+
+        # To Find The Target Node
         while curr:
-            if curr.data == prevNode:
-                prevNode = curr
+            if curr.data == key:
+                target = curr
                 break
             curr = curr.nextNode 
-        
-        if prevNode is None:
+
+        # To Check The Target
+        if target is None:
             print("The Previous Node Cannot Be Null.")
             return False
+
+        # Creating New Node
         newNode = Node(data)
 
-        newNode.prevNode = prevNode
-        prevNode.nextNode = newNode
+        # Setting Next Node
+        if target.prevNode is not None:
+            target.prevNode.nextNode = newNode
+        newNode.nextNode = target
+
+        # Setting Previous Node
+        newNode.prevNode = target.prevNode
+        target.prevNode = newNode
 
         return True
         
     def printList(self):
         curr = self.head
+
         while curr:
-            print(curr.data)
-            curr = curr.nextNode 
+            print(curr.data,end=" --> ")
+            curr = curr.nextNode
+        print("None")
+
+#  Creating The List And Adding Elements
+l = DoubleLinkedList()
+l.push(1)
+l.push(3)
+l.push(4)
+l.push(5)
+
+# Inserting Element
+print("Before Inserting Element")
+l.printList()
+l.insert(1,2)
+print("After Inserting Element")
+l.printList()
+
+# Checking Funtion If It Works For Base Case
+l.insert(5,6)
+l.printList()
