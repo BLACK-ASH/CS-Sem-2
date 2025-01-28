6ee1be5639b673ba24f6c240ece52be12593fe8f
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2025-01-28 20:23:36 +0530

    DAA Practicals Added

--- DAA/Practical Code/queue.py
+++ DAA/Practical Code/queue.py
@@ -18,3 +18,23 @@ class Queue:
 
     def print(self):
         print(self.queue)
+
+queue = Queue()
+
+# Adding Elements
+queue.enque(1)
+queue.enque(2)
+queue.enque(3)
+queue.enque(4)
+queue.enque(5)
+queue.print()
+
+# Removing Elements
+queue.deque()
+queue.deque()
+queue.deque()
+queue.print()
+
+# Checking If Queue Is Empty And Length Of Queue
+print(queue.isEmpty())
+print(queue.length())
