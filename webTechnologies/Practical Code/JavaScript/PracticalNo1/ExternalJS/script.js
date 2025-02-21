af3c810f09c7873d69ecde83567584dafe8edc31
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2025-02-21 20:32:39 +0530

    JavaScript P1 Added

---
+++ webTechnologies/Practical Code/JavaScript/PracticalNo1/ExternalJS/script.js
@@ -0,0 +1,7 @@
+document.write("<b>Now I am using External JavaScript</b>");
+document.write("<br>");
+document.write("Hello World");
+document.write("<br>");
+// Comment In JavaScript
+// Date Module Gives Current Date
+document.write(Date());
